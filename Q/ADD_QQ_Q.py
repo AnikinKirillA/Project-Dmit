363451db6d7fbdacfa36a703899bab2451baebea
npimkhovik_24 <imknatalia@mail.ru>
2025-11-06 23:45:43 +0300

    Q: ADD_QQ_Q method

---
+++ Q/ADD_QQ_Q.py
@@ -0,0 +1,18 @@
+def ADD_QQ_Q(A: Rational, B: Rational) -> Rational:
+    """
+    Сделала: Имховик Наталья
+    Выполняет сложение дробей
+    Возвращает дробь
+    """
+    # Находим НОК знаменателей
+    lcm = LCM_NN_N(A.denominator, B.denominator)
+
+    # Находим дополнительные множители
+    multiplyerA = lcm // A.denominator
+    multiplyerB = lcm // B.denominator
+
+    # Умножаем числители на дополнительные множители
+    newNumerator = A.numerator * multiplyerA + B.numerator * multiplyerB
+
+    # Создаем результирующую дробь
+    return Rational(newNumerator, lcm)
