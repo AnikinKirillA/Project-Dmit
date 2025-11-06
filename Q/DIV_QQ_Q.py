6fb18f9b186e4ccad19104e3be1e18ee74734a81
GGraff1k <thedarkorgrey@gmail.com>
2025-11-06 18:29:28 +0300

    Chumakov_Nikita_Q

--- Q/DIV_QQ_Q.py
+++ Q/DIV_QQ_Q.py
@@ -1,4 +1,4 @@
-def DIV_QQ_Q(q1: Rational, q2: Rational) -> Rational:
+def __truediv__(self, other):
     """
     Сделал: Чумаков Никита Ярославович
     Деление рациональных чисел q1 / q2.
@@ -6,12 +6,12 @@ def DIV_QQ_Q(q1: Rational, q2: Rational) -> Rational:
     Возвращает новый Rational.
     """
     # Проверим, что делитель не равен нулю (числитель делителя != 0)
-    if all(d == 0 for d in q2.numerator.A):
+    if all(d == 0 for d in other.numerator.A):
         raise ZeroDivisionError("Деление на ноль в рациональных числах")
 
     # По формуле: (a/b) ÷ (c/d) = (a*d) / (b*c) перемножаем
-    new_numerator = MUL_ZZ_Z(q1.numerator, TRANS_N_Z(q2.denominator))
-    new_denominator = MUL_NN_N(q1.denominator, ABS_Z_N(q2.numerator))
+    new_numerator = self.numerator * RANS_N_Z(other.denominator)
+    new_denominator = self.denominator * ABS_Z_N(other.numerator)
 
     # Формируем новое рациональное число
     result = Rational(new_numerator, new_denominator)
--- Q/MUL_QQ_Q.py
+++ Q/MUL_QQ_Q.py
@@ -1,13 +1,11 @@
-def MUL_QQ_Q(q1: Rational, q2: Rational) -> Rational:
+def __mul__(self, other):
     """
     Сделал: Чумаков Никита Ярославович
     Умножение двух рациональных чисел.
-    q1, q2 — объекты класса Rational.
     Результат — новый Rational.
     """
-    # Умножаем числители и знаменатели
-    new_numerator = MUL_ZZ_Z(q1.numerator, q2.numerator)
-    new_denominator = MUL_NN_N(q1.denominator, q2.denominator) # здесь надо использовать MUL_NN_N, но по условию она не включена??
+    new_numerator = self.numerator * other.numerator
+    new_denominator = self.denominator * other.denominator
 
     # Формируем новую дробь
     result = Rational(new_numerator, new_denominator)
