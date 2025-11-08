7b49e4a4062173bb463f74a8d6ba6e6f88d597fd
mvsurin_24 <uloxvk@gmail.com>
2025-11-09 00:07:11 +0300

    Q fixed

--- Q/RED_Q_Q.py
+++ Q/RED_Q_Q.py
@@ -12,9 +12,10 @@ def RED_Q_Q(self):
     gcf = Natural(self.numerator.len, self.numerator.A).GCF_NN_N(self.denominator)
     gcf = Integer(0, gcf.len, gcf.A)
 
+    new_ratio = self.deepcopy()
     """ Если НОД не равен 1, сокращаем на него числитель и знаменатель """
     if gcf != 1:
-        self.numerator //= gcf
-        self.denominator //= gcf
+        new_ratio.numerator = new_ratio.numerator // gcf
+        new_ratio.denominator = new_ratio.denominator // gcf
 
-    return self
+    return new_ratio
